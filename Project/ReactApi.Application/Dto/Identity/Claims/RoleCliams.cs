301ef84da0d2eff0866d11de3b3dbd98ec139cce
iltaf <abidiltaf21@gmail.com>
2026-06-18 09:42:38 +0200

    Remove old Project folder

--- Project/ReactApi.Application/Dto/Identity/Claims/RoleCliams.cs
+++
@@ -1,8 +0,0 @@
-namespace ReactApi.Application.Dto.Identity.Claims
-{
-    public class RoleCliams
-    {
-        public string? ClaimType { get; set; }
-        public bool IsSelected { get; set; }
-    }
-}
--- Project/ReactApi.Domin/Commond model/UpdateClientRequestDto.cs
+++
@@ -1,9 +0,0 @@
-namespace ReactApi.Domin.Commond_model
-{
-    public class UpdateClientRequestDto
-    {
-        public string? Status { get; set; }
-        public string? InternalNotes { get; set; }
-        public bool? IsRead { get; set; }
-    }
-}
